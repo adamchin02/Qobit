e8820239a9f6695b4e5bf42864cfec5466fffc95
ashimida6 <ashimida6@github.com>
2019-04-23 11:13:19 +0800

    update timelock

--- contracts/Timelock.sol
+++ contracts/Timelock.sol
@@ -1,36 +1,47 @@
 pragma solidity 0.4.24;
 
-interface ERC20Token {
+interface IERC20 {
     function transfer(address _to, uint256 _value) external returns (bool success);
     function balanceOf(address _owner) external view returns (uint256 balance);
 }
 
 contract Timelock {
-    ERC20Token public token;
-    address public beneficiary;
-    uint256 public releaseTime;
+    IERC20 private _token;
+    address private _beneficiary;
+    uint256 private _releaseTime;
 
     event TokenReleased(address beneficiary, uint256 amount);
 
     constructor(
-        address _token,
-        address _beneficiary
-        // ,
-        // uint256 _releaseTime
+        address token,
+        address beneficiary,
+        uint256 releaseTime
     ) public {
-        // require(_releaseTime > now);
-        require(_beneficiary != 0x0);
-        token = ERC20Token(_token);
-        beneficiary = _beneficiary;
-        // releaseTime = _releaseTime;
+        require(releaseTime > block.timestamp);
+        require(beneficiary != 0x0);
+        _token = IERC20(token);
+        _beneficiary = beneficiary;
+        _releaseTime = releaseTime;
     }
 
-    function release() public returns(bool success) {
-        // require(now >= releaseTime);
-        uint256 amount = token.balanceOf(this);
+    function release() public {
+        require(block.timestamp >= _releaseTime);
+        uint256 amount = _token.balanceOf(address(this));
         require(amount > 0);
-        token.transfer(beneficiary, amount);
-        emit TokenReleased(beneficiary, amount);
-        return true;
+        _token.transfer(_beneficiary, amount);
+        emit TokenReleased(_beneficiary, amount);
+    }
+
+    
+    function token() public view returns(IERC20) {
+        return _token;
+    }
+
+    function beneficiary() public view returns(address) {
+        return _beneficiary;
+    }
+
+    function releaseTime() public view returns(uint256) {
+        return _releaseTime;
     }
 }
